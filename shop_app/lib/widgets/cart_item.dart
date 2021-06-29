13dd3edd77262994574778e8fa6cb32203f7f6cd
TKisely <tamaskisely@gmail.com>
2021-06-29 14:15:01 +0200

    cart delete icon added

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -14,21 +14,41 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(
-                child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text('\$${price}'),
-            )),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          alignment: Alignment.centerRight,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.grey.shade200,
+            ),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text(quantity == 1 ? '${quantity} pc' : '${quantity} pcs'),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              color: Theme.of(context).errorColor),
+        ),
+      ),
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(
+                  child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text('\$${price}'),
+              )),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing:
+                Text(quantity == 1 ? '${quantity} pc' : '${quantity} pcs'),
+          ),
         ),
       ),
     );
