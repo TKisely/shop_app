791d24be0db17320835a024f7178b718e1278769
TKisely <tamaskisely@gmail.com>
2021-06-30 17:03:05 +0200

    order expand added

--- shop_app/lib/widgets/order_item.dart
+++ shop_app/lib/widgets/order_item.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/orders.dart';
 
-class SingleOrder extends StatelessWidget {
+class SingleOrder extends StatefulWidget {
   final OrderItem order;
   const SingleOrder({Key? key, required this.order}) : super(key: key);
+
+  @override
+  _SingleOrderState createState() => _SingleOrderState();
+}
+
+class _SingleOrderState extends State<SingleOrder> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +21,42 @@ class SingleOrder extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$' + order.amount.toString()),
-            subtitle: Text(order.dateTime.toString()),
-            trailing:
-                IconButton(onPressed: () {}, icon: Icon(Icons.expand_more)),
-          )
+            title: Text('\$' + widget.order.amount.toString()),
+            subtitle: Text(widget.order.dateTime.toString()),
+            trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more)),
+          ),
+          if (_expanded)
+            Container(
+                height: min(widget.order.products.length * 20 + 100, 140),
+                child: ListView.builder(
+                    itemCount: widget.order.products.length,
+                    itemBuilder: (ctx, i) => Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 4),
+                              child: Text(
+                                widget.order.products[i].title.toString(),
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Spacer(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 16, vertical: 4),
+                              child: Text(widget.order.products[i].quantity
+                                      .toString() +
+                                  ' x \$' +
+                                  widget.order.products[i].price.toString()),
+                            )
+                          ],
+                        )))
         ],
       ),
     );
